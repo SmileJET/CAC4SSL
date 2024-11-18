f792d9118f9b9b173e4a39adb7dee94f3bc3778e
Lei Li <lilei@hebtu.edu.cn>
2024-11-18 08:16:08 +0800

    Update unet_mso.py

--- code/networks/unet_mso.py
+++ code/networks/unet_mso.py
@@ -132,36 +132,6 @@ class Encoder(nn.Module):
         x4 = self.down4(x3)
         return [x0, x1, x2, x3, x4]
     
-    
-class Encoder(nn.Module):
-    def __init__(self, params):
-        super(Encoder, self).__init__()
-        self.params = params
-        self.in_chns = self.params['in_chns']
-        self.ft_chns = self.params['feature_chns']
-        self.n_class = self.params['class_num']
-        self.bilinear = self.params['bilinear']
-        self.dropout = self.params['dropout']
-        assert (len(self.ft_chns) == 5)
-        self.in_conv = ConvBlock(
-            self.in_chns, self.ft_chns[0], self.dropout[0])
-        self.down1 = DownBlock(
-            self.ft_chns[0], self.ft_chns[1], self.dropout[1])
-        self.down2 = DownBlock(
-            self.ft_chns[1], self.ft_chns[2], self.dropout[2])
-        self.down3 = DownBlock(
-            self.ft_chns[2], self.ft_chns[3], self.dropout[3])
-        self.down4 = DownBlock(
-            self.ft_chns[3], self.ft_chns[4], self.dropout[4])
-
-    def forward(self, x):
-        x0 = self.in_conv(x)
-        x1 = self.down1(x0)
-        x2 = self.down2(x1)
-        x3 = self.down3(x2)
-        x4 = self.down4(x3)
-        return [x0, x1, x2, x3, x4]
-    
 
 class Decoder(nn.Module):
     ''' Decoder with multi scale outputs '''
